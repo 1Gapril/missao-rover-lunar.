133c5a65b11de4dc675c9db7949a1d2f73f7de28
Gabriel Ferreira de Souza <gabrielferreirapvz2@gmail.com>
2026-08-28 21:28:09 -0300

    Initialize Rover system in rover.java

--- rover.java
+++ rover.java
@@ -1,1 +1,11 @@
-
+// Rover.java
+public class Rover {
+public static void inicializarRover() {
+System.out.println("Sistemas do Rover iniciados!");
+System.out.println("Painéis solares: OK");
+System.out.println("Nível de bateria: 100%");
+}
+public static void main(String[] args) {
+inicializarRover();
+}
+}
